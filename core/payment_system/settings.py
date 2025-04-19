cb56aa2810d46aa41a16440c24cd7b9e9c79f631
Sadeesha-Sath <sadeerc@gmail.com>
2025-04-19 22:10:34 +0530

    Add initial migrations

--- core/payment_system/settings.py
+++ core/payment_system/settings.py
@@ -2,12 +2,12 @@ import os
 from pathlib import Path
 import environ
 
-# Initialize environ
-env = environ.Env()
-environ.Env.read_env()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Initialize environ
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
